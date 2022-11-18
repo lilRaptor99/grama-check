f461469b066ca0bb28fcffa36d733d86808a4316
npathirana1 <nethmi.pathirana@gmail.com>
2022-11-18 15:20:50 +0530

    Check status page

--- web-app/src/pages/check-status.js
+++ web-app/src/pages/check-status.js
@@ -1,9 +1,25 @@
-const CheckStatus = () => {
-    return (
-        <div>
-            <h3>Check Status</h3>
-        </div>
-    );
-};
+import * as React from "react";
+import PendingImg from "../assets/images/Pending.png";
+import { Container, Row } from "react-bootstrap";
 
-export default CheckStatus;
+export default function CheckStatus() {
+  return (
+    <Container className="check-status">
+      <Row>
+        <h5 style={{ color: "#42A5F5" }}>Check Status</h5>
+      </Row>
+      <Row className="justify-content-center">
+        <img
+          style={{ width: 300, height: 300 }}
+          src={PendingImg}
+        />
+      </Row>
+      <Row>
+        <h3 className="">The status of your application is </h3>
+      </Row>
+      <Row>
+        <h2 className="text-center">Pending</h2>
+      </Row>
+    </Container>
+  );
+}
