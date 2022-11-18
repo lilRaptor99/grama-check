d2ab09f5ca3750a5964e9256407bca67d1f5132a
azeimtiaz <imtiaz.azma@gmail.com>
2022-11-18 14:45:21 +0530

    add logout button to landing page

--- web-app/src/pages/home.js
+++ web-app/src/pages/home.js
@@ -1,12 +1,20 @@
 import { Container, Row, Col } from 'react-bootstrap';
-import React from "react";
+import React, { useState } from "react";
 import { useAuthContext } from "@asgardeo/auth-react";
 import Button from '@mui/material/Button';
 
 const Home = () => {
-    const { signIn } = useAuthContext();
+    const { signIn, signOut, state } = useAuthContext();
+    const [token, setToken] = useState(JSON.parse(localStorage.getItem('token')));
+
     const handleLogin = () => {
         signIn();
+    }
+
+    const handlLogout = () => {
+        signOut();
+        localStorage.removeItem('token');
+        setToken(null);
     }
 
     return (
@@ -16,7 +24,11 @@ const Home = () => {
             </Row>
             <Row className="w-50 mx-auto">
                 <Row className="w-50 mx-auto justify-content-center">
-                    <Button variant="contained" size="small" sx={{ width: 100, }} onClick={handleLogin}>Login</Button>
+                    {
+                        token || state?.isAuthenticated ?
+                            <Button variant="outlined" size="small" sx={{ width: 100, }} onClick={handlLogout}>Logout</Button>
+                            : <Button variant="contained" size="small" sx={{ width: 100, }} onClick={handleLogin}>Login</Button>
+                    }
                 </Row>
             </Row>
         </Container>
