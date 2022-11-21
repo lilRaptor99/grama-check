5fe7f8f1df4184e16934cc8d23c337c5d85c386d
azeimtiaz <imtiaz.azma@gmail.com>
2022-11-21 08:32:12 +0530

    remove config.js

--- web-app/src/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  signInRedirectURL: 'https://localhost:3000/menu',
-  signOutRedirectURL: 'https://localhost:3000',
-  clientID: 'kPd7Cg_4E8hxkmqL0xNnHCoLxh8a',
-  baseUrl: 'https://api.asgardeo.io/t/azma',
-  scope: ['openid', 'profile'],
-};
-
-export default config;
