8deaf06489267243839142a4e60414ddc48a26ae
azeimtiaz <imtiaz.azma@gmail.com>
2022-11-23 11:17:24 +0530

    integrate asgardeo login

--- admin-portal/src/config.js
+++ admin-portal/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  signInRedirectURL: `${process.env.REACT_APP_WEB_URL}/home`,
+  signInRedirectURL: `${process.env.REACT_APP_WEB_URL}/dashboard`,
   signOutRedirectURL: `${process.env.REACT_APP_WEB_URL}/home`,
   clientID: process.env.REACT_APP_ASGARDEO_CLIENT_ID,
   baseUrl: process.env.REACT_APP_ASGARDEO_BASE_URL,
